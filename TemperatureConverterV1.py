019d7380d35c392a0f31c47253f14575b4e640eb
Yoshi <93171045+yoshibrownies@users.noreply.github.com>
2025-06-11 13:40:22 +1200

    All frames working but to farenheit conversion not showing

--- TemperatureConverterV1.py
+++ TemperatureConverterV1.py
@@ -12,38 +12,107 @@ class Converter:
 
         # Container for frames
         self.container=Frame(self.master)
+        self.container.pack()
 
+        # Dictionary holding all frames
         self.frames={}
         self.frames['main_frame']=self.create_main_frame()
-        self.show_frame(self.frames['main_frame'])
+        self.frames['to_f']=self.create_to_f()
+        self.frames['to_c']=self.create_to_c()
 
-        
-
+        # Showing Menu Frame
+        self.show_frame('main_frame')
 
     def run(self):
         # Run program
         self.master.mainloop()
+
     def show_frame(self, name):
         # Display window
-        frame = self.frames[name] # Gets frame
+        frame = self.frames[name]# Gets frame
         frame.tkraise() # Brings frame to top
+
     def create_main_frame(self):
         frame=Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nswe')
 
         self.l_title=Label(frame, text='Temperature Converter', font='Arial 20 bold')
-        self.l_title.pack(fill=X)
+        self.l_title.grid(columnspan=2, row=0, sticky='nsew')
 
-        # Bottom Frame and Converter Buttons
-        
-        self.b_celcius = Button(frame, text='to Celcius', font='Arial 15 bold', bg='yellow')
-        self.b_celcius.pack(side=LEFT)
+        # Converter Buttons
+        self.b_celcius = Button(frame, text='to Celcius', font='Arial 15 bold', bg='yellow', command=lambda:self.show_frame('to_c'))
+        self.b_celcius.grid(row=1, column=0,sticky='nsew')
 
-        self.b_farenheit = Button(frame, text='to Farenheit', font='Arial 15 bold', bg='red')
-        self.b_farenheit.pack(side=LEFT)
+        self.b_farenheit = Button(frame, text='to Farenheit', font='Arial 15 bold', bg='red', command=lambda:self.show_frame('to_f'))
+        self.b_farenheit.grid(row=1, column=1,sticky='nsew')
+
+        return frame
+    
+    def create_to_f(self):
+        frame=Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nswe')
+
+        # Title
+        self.l_title=Label(frame, text='Enter Temperature in Celcius', font='Arial 15 bold')
+        self.l_title.grid(columnspan=3, row=0, sticky='nsew')
+
+        # User Entry 
+        self.e_celcius= Entry(frame, justify=CENTER)
+        self.e_celcius.grid(row=1, columnspan=3,sticky='ew')
+
+        # Buttons
+        self.b_calc = Button(frame, text='Calculate', font='Arial 10', command=lambda:self.calculate_to('f'))
+        self.b_calc.grid(row=2, column=0,sticky='nsew')
+
+        self.b_calc = Button(frame, text='Back', font='Arial 10', command=lambda:self.show_frame('main_frame'))
+        self.b_calc.grid(row=2, column=1,sticky='nsew')
+
+        self.b_calc = Button(frame, text='Reset', font='Arial 10', command=lambda:self.e_celcius.delete(0,END))
+        self.b_calc.grid(row=2, column=2,sticky='nsew')
+
+        # Calculated Temperature
+        self.l_temperature=Label(frame, text='Converted Temperature Goes Here', font='Arial 8')
+        self.l_temperature.grid(columnspan=3, row=3, sticky='nsew')
+
+        return frame
+    
+    def create_to_c(self):
+        frame=Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nswe')
+
+        # Title
+        self.l_title=Label(frame, text='Enter Temperature in Farenheit', font='Arial 15 bold')
+        self.l_title.grid(columnspan=3, row=0, sticky='nsew')
+
+        # User Entry 
+        self.e_farenheit= Entry(frame, justify=CENTER)
+        self.e_farenheit.grid(row=1, columnspan=3,sticky='ew')
+
+        # Buttons
+        self.b_calc = Button(frame, text='Calculate', font='Arial 10', command=lambda:self.calculate_to('c'))
+        self.b_calc.grid(row=2, column=0,sticky='nsew')
+
+        self.b_calc = Button(frame, text='Back', font='Arial 10', command=lambda:self.show_frame('main_frame'))
+        self.b_calc.grid(row=2, column=1,sticky='nsew')
+
+        self.b_calc = Button(frame, text='Reset', font='Arial 10', command=lambda:self.e_celcius.delete(0,END))
+        self.b_calc.grid(row=2, column=2,sticky='nsew')
+
+        # Calculated Temperature
+        self.l_temperature=Label(frame, text='Converted Temperature Goes Here', font='Arial 8')
+        self.l_temperature.grid(columnspan=3, row=3, sticky='nsew')
 
         return frame
 
+    def calculate_to(self, temperature_type):
+        try:
+            if temperature_type=='f':
+                self.l_temperature.configure(text=str(float(self.e_celcius.get())*1.8+32)+'F')
+            elif temperature_type=='c':
+                self.l_temperature.configure(text=str((float(self.e_farenheit.get())-32)/1.8+32)+'C')
 
+        except ValueError:
+            self.l_temperature.configure(text='Enter Only Numbers')
 
 app=Converter()
 app.run()
